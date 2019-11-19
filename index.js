ceb67a13f3acd568c35b5881c08c9ce1d3c9334c
Craig Donavin <crdonavin@gmail.com>
2019-11-19 20:36:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ function testFunc() {
 
 
 function superbowlWin(array){
-  array.find(function(el) {
     let record = array.find(record => record.result === "W")
-    if (!record) {
-      return
-    } else {
-      return record.year
-    }
-  }
+      if (!record) {
+        return
+      } else {
+        return record.year
+      }
+  
 }
